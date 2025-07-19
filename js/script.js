24befba271a2453ebd4074a3edc39ac97cfd0c71
arg0nis <mrlng.simon@gmail.com>
2025-07-19 14:43:58 +0200

    Added dot navigation animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello from JS!")
+const dots = document.querySelectorAll('.dot');
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            const id = entry.target.getAttribute('id');
+            dots.forEach(dot => {
+                dot.classList.toggle('active', dot.getAttribute('href').slice(1) === id);
+            });
+        }
+    });
+}, {threshold: 0.6});
+
+sections.forEach(section => observer.observe(section));
